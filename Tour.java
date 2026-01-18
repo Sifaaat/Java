8b31752c55e87ea43dd0edd386a6cd0c355a6ac9
Sifat Ahmed <sifathossin472@gmail.com>
2026-01-18 23:05:32 +0600

    Add files via upload

---
+++ Tour.java
@@ -0,0 +1,11 @@
+
+package tour;
+public class Tour {
+  String location;
+
+  public static void main(String[] args) {
+    Tour tour1 = new Tour(); 
+     tour1.location ="Saint Martin";
+    System.out.println(tour1.location);
+  }
+}
