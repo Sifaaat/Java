19ffcc1631dcdd2cc7e6cde63ab83ed3131c975f
Sifat Ahmed <sifathossin472@gmail.com>
2026-01-26 01:31:09 +0600

    Add files via upload

---
+++ ClassObject.java
@@ -0,0 +1,11 @@
+package pkgclass.object;
+public class ClassObject{
+String name ="Sifat Ahmed";
+        int age =22;
+                
+public static void main(String[] args){
+    ClassObject Identity = new ClassObject();
+    System.out.println(Identity.name);
+    System.out.println(Identity.age);
+   }
+}
