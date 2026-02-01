0db146c5b30d9044617b489deb98a3d55a9cdaa7
Sifat Ahmed <sifathossin472@gmail.com>
2026-02-01 22:51:15 +0600

    Implement basic voting eligibility check

---
+++ 11_If Else_VotingSystem.java
@@ -0,0 +1,11 @@
+public class VotingSystem {
+public static void main(String[] args){
+int MyAge = 25;
+int VotingAge = 18;
+
+
+if(MyAge>=VotingAge){
+System.out.println("Old enough to vote");}
+ else{
+System.out.println("Not available to vote");
+}}}
